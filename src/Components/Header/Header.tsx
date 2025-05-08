401dd252a016c277f3572ffd70762eff9c46ffaa
Rasel Hossain Adib <97701792+AdibRasel@users.noreply.github.com>
2025-05-08 19:04:35 +0600

    github g-page add

--- src/Components/Header/Header.tsx
+++ src/Components/Header/Header.tsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 import Logo from "../../assets/Logo/Logo.png"
 import { Link } from 'react-router-dom'
@@ -14,79 +14,269 @@ import MailOutlineIcon from '@mui/icons-material/MailOutline';
 import NotificationsNoneOutlinedIcon from '@mui/icons-material/NotificationsNoneOutlined';
 import LightModeOutlinedIcon from '@mui/icons-material/LightModeOutlined';
 
+
+import Box from '@mui/material/Box';
+import Avatar from '@mui/material/Avatar';
+import Menu from '@mui/material/Menu';
+import MenuItem from '@mui/material/MenuItem';
+import ListItemIcon from '@mui/material/ListItemIcon';
+import Divider from '@mui/material/Divider';
+import IconButton from '@mui/material/IconButton';
+import Typography from '@mui/material/Typography';
+import Tooltip from '@mui/material/Tooltip';
+import PersonAdd from '@mui/icons-material/PersonAdd';
+import AccountCircleIcon from '@mui/icons-material/AccountCircle';
+import AccountCircleOutlinedIcon from '@mui/icons-material/AccountCircleOutlined';
+import Settings from '@mui/icons-material/Settings';
+import RestoreIcon from '@mui/icons-material/Restore';
+import Logout from '@mui/icons-material/Logout';
+
+import Config from '../../../Config.json';
+import SideMenu from './SideMenu';
+
+
+
 const Header = () => {
-  return (
-    <>
-    
-    <header className='d-flex align-items-center'>
-
-        <div className="container-fluid w-100">
-            <div className="row d-flex align-items-center">
 
 
-                {/* First Section Logo  ===== part 1 ====*/}
-                <div className="col-sm-2 part1">
-                    <Link to="/" className='d-flex align-items-center logo'>
-                        <img src={Logo}  alt="Logo" />
-                        <span className='ml-2'>KEF Online</span>
-                    </Link>
-                </div>
+    // My Account Dropdown
+    const [anchorEl, setAnchorEl] = React.useState<null | HTMLElement>(null);
+    const open = Boolean(anchorEl);
+    const handleClickMyAccDrop = (event: React.MouseEvent<HTMLElement>) => {
+        setAnchorEl(event.currentTarget);
+    };
+    const handleCloseMyAccDrop = () => {
+        setAnchorEl(null);
+    };
+   
+   
+    // Notification Dropdown
+    const [anchorElNotificationsDrop, setNotificationsDropAnchorEl] = React.useState<null | HTMLElement>(null);
+    const openNotificationsDrop = Boolean(anchorElNotificationsDrop);
+    const handleClickNotificationsDrop = (event: React.MouseEvent<HTMLElement>) => {
+        setNotificationsDropAnchorEl(event.currentTarget);
+    };
+    const handleCloseNotificationsDrop = () => {
+        setNotificationsDropAnchorEl(null);
+    };
 
 
+    return (
+        <>
+
+            <header className='d-flex align-items-center'>
+
+                <div className="container-fluid w-100">
+                    <div className="row d-flex align-items-center">
 
 
-
-                {/* Second Section Search Bar Wrapper ===== part 2 ===== */}
-                <div className="col-sm-3 d-flex align-items-center part2">
-                    <Button className='rounded-circle mr-3'><MenuOpenIcon/></Button>
-                    <SearchBox />
-                </div>
-
-
-
-                
-                {/* Second Section Icons Wrapper ===== part 3 ===== */}
-                <div className="col-sm-7 d-flex align-items-center justify-content-end part3">
-                    <Button className='rounded-circle mr-3'><LightModeOutlinedIcon/></Button>
-                    <Button className='rounded-circle mr-3'><ShoppingCartOutlinedIcon/></Button>
-                    <Button className='rounded-circle mr-3'><MailOutlineIcon/></Button>
-                    <Button className='rounded-circle mr-3'><NotificationsNoneOutlinedIcon/></Button>
-
-
-                    <Button className="myAccWrapper">
-                        <div className="myAcc d-flex align-items-center">
-                            <div className="userImg">
-                                <span className='rounded-circle'>
-                                    <img src="https://avatars.githubusercontent.com/u/97701792?v=4" alt="" />
-                                </span>
-                            </div>
-
-
-                            <div className="userInfo">
-                            <h4>Rasal Hossain</h4>
-                            <p className='mb-0'>@rasal</p>
-                        </div>
+                        {/* First Section Logo  ===== part 1 ====*/}
+                        <div className="col-sm-2 part1">
+                            <Link to="/" className='d-flex align-items-center logo'>
+                                <img src={Logo} alt="Logo" />
+                                <span className='ml-2'>{Config.Name}</span>
+                                
+                            </Link>
                         </div>
 
-                    
-
-
-                    </Button>
 
 
 
 
+                        {/* Second Section Search Bar Wrapper ===== part 2 ===== */}
+                        <div className="col-sm-3 d-flex align-items-center part2">
+                            <Button className='rounded-circle mr-3'><MenuOpenIcon /></Button>
+                            <SearchBox />
+                        </div>
+
+
+
+
+                        {/* Second Section Icons Wrapper ===== part 3 ===== */}
+                        <div className="col-sm-7 d-flex align-items-center justify-content-end part3">
+
+                            <Button className='rounded-circle mr-3'><LightModeOutlinedIcon /></Button>
+
+
+
+                            <Button className='rounded-circle mr-3'><ShoppingCartOutlinedIcon /></Button>
+
+
+                            <Button className='rounded-circle mr-3'><MailOutlineIcon /></Button>
+
+                            {/* Notification Start  */}
+                            <Button onClick={handleClickNotificationsDrop} className='rounded-circle mr-3'><NotificationsNoneOutlinedIcon /></Button>
+                            <Menu
+                                anchorEl={anchorElNotificationsDrop}
+                                className="notifications"
+                                id="notifications"
+                                open={openNotificationsDrop}
+                                onClose={handleCloseNotificationsDrop}
+                                onClick={handleCloseNotificationsDrop}
+                                slotProps={{
+                                    paper: {
+                                        elevation: 0,
+                                        sx: {
+                                            overflow: 'visible',
+                                            filter: 'drop-shadow(0px 2px 8px rgba(0,0,0,0.32))',
+                                            mt: 1.5,
+                                            '& .MuiAvatar-root': {
+                                                width: 32,
+                                                height: 32,
+                                                ml: -0.5,
+                                                mr: 1,
+                                            },
+                                            '&::before': {
+                                                content: '""',
+                                                display: 'block',
+                                                position: 'absolute',
+                                                top: 0,
+                                                right: 14,
+                                                width: 10,
+                                                height: 10,
+                                                bgcolor: 'background.paper',
+                                                transform: 'translateY(-50%) rotate(45deg)',
+                                                zIndex: 0,
+                                            },
+                                        },
+                                    },
+                                }}
+                                transformOrigin={{ horizontal: 'right', vertical: 'top' }}
+                                anchorOrigin={{ horizontal: 'right', vertical: 'bottom' }}
+                            >
+
+                                <div className="head p-2 pb-0 d-flex align-items-center justify-content-between">
+                                    <span className='font-bold'>Today's Order (12) </span>
+                                    <span>See All</span>
+                                </div>
+
+                                <Divider className='mb-3' />
+
+
+                                <MenuItem onClick={handleCloseNotificationsDrop}>
+                                    <div className="d-flex align-items-center NotificationsItem">
+                                        <div className="userImg">
+                                            <span className='rounded-circle'>
+                                                <img src="https://avatars.githubusercontent.com/u/97701792?v=4" alt="" />
+                                            </span>
+                                        </div>
+
+                                        <p>Headphon g54 &nbsp;&nbsp;&nbsp; </p> <br />
+                                        <p>  1250/-</p>
+
+                                    </div>
+                                </MenuItem>
+                            </Menu>
+
+                            {/* Notification End */}
+
+
+
+
+
+                            {/* My Account Start  */}
+                            <Button className="myAccWrapper" onClick={handleClickMyAccDrop}>
+                                <div className="myAcc d-flex align-items-center">
+                                    <div className="userImg">
+                                        <span className='rounded-circle'>
+                                            <img src="https://avatars.githubusercontent.com/u/97701792?v=4" alt="" />
+                                        </span>
+                                    </div>
+
+
+                                    <div className="userInfo">
+                                        <h4>Rasal Hossain</h4>
+                                        <p className='mb-0'>@rasal</p>
+                                    </div>
+                                </div>
+
+
+
+
+                            </Button>
+                            <Menu
+                                anchorEl={anchorEl}
+                                id="account-menu"
+                                open={open}
+                                onClose={handleCloseMyAccDrop}
+                                onClick={handleCloseMyAccDrop}
+                                slotProps={{
+                                    paper: {
+                                        elevation: 0,
+                                        sx: {
+                                            overflow: 'visible',
+                                            filter: 'drop-shadow(0px 2px 8px rgba(0,0,0,0.32))',
+                                            mt: 1.5,
+                                            '& .MuiAvatar-root': {
+                                                width: 32,
+                                                height: 32,
+                                                ml: -0.5,
+                                                mr: 1,
+                                            },
+                                            '&::before': {
+                                                content: '""',
+                                                display: 'block',
+                                                position: 'absolute',
+                                                top: 0,
+                                                right: 14,
+                                                width: 10,
+                                                height: 10,
+                                                bgcolor: 'background.paper',
+                                                transform: 'translateY(-50%) rotate(45deg)',
+                                                zIndex: 0,
+                                            },
+                                        },
+                                    },
+                                }}
+                                transformOrigin={{ horizontal: 'right', vertical: 'top' }}
+                                anchorOrigin={{ horizontal: 'right', vertical: 'bottom' }}
+                            >
+                                <MenuItem onClick={handleCloseMyAccDrop}>
+                                    <ListItemIcon>
+                                        <AccountCircleOutlinedIcon fontSize="small" />
+                                    </ListItemIcon>
+                                    My Account
+                                </MenuItem>
+                                <MenuItem onClick={handleCloseMyAccDrop}>
+                                    <ListItemIcon>
+                                        <RestoreIcon fontSize="small" />
+                                    </ListItemIcon>
+                                    Reset Password
+                                </MenuItem>
+                                <MenuItem onClick={handleCloseMyAccDrop}>
+                                    <ListItemIcon>
+                                        <Logout fontSize="small" />
+                                    </ListItemIcon>
+                                    Logout
+                                </MenuItem>
+                            </Menu>
+                            {/* My Account End  */}
+
+
+
+
+                        </div>
+
+
+                    </div>
                 </div>
 
+            </header>
+            
 
-            </div>
-        </div>
 
-    </header>
-    
-    
-    </>
-  )
+            <SideMenu />
+
+
+
+
+
+
+            
+
+
+        </>
+    )
 }
 
 export default Header
---
+++ src/Components/Header/SideMenu.tsx
@@ -0,0 +1,58 @@
+import React from 'react'
+
+const SideMenu = () => {
+    return (
+        <>
+
+        <div className="d-flex">
+
+            <div className="SideMenu">
+                <div className="MenuItem">
+                    <span className='active'>Dashboard</span>
+                </div>
+                <div className="MenuItem">
+                    <span className='active'>Category</span>
+                </div>
+                <div className="MenuItem">
+                    <span className='active'>Sub Category</span>
+                </div>
+                <div className="MenuItem">
+                    <span className='active'>Brands</span>
+                </div>
+                <div className="MenuItem">
+                    <span className='active'>Variants</span>
+                </div>
+                <div className="MenuItem">
+                    <span className='active'>Product</span>
+                </div>
+                <div className="MenuItem">
+                    <span className='active'>Order</span>
+                </div>
+                <div className="MenuItem">
+                    <span className='active'>Coupons</span>
+                </div>
+                <div className="MenuItem">
+                    <span className='active'>Posters</span>
+                </div>
+                <div className="MenuItem">
+                    <span className='active'>Notification</span>
+                </div>
+                <div className="MenuItem">
+                    <span className='active'>Settings</span>
+                </div>
+            </div>
+
+           <div className="dahbord">
+           <h4>Dashboard</h4>
+
+           
+           </div>
+
+           </div>
+
+
+        </>
+    )
+}
+
+export default SideMenu
